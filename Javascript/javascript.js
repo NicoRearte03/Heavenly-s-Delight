33284d1d7f574b0cd0cc60ef76979e844b9883d2
Nicolás Rearte <nicolas.rearte455@gmail.com>
2024-05-04 13:06:54 -0300

    $ git commit -m "Guardar los archivos correctos"

---
+++ Javascript/javascript.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var currentURL = window.location.href;
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].href === currentURL) {
+            links[i].style.backgroundColor = '#FFB6C1';
+        }
+    }
+});
+
+
